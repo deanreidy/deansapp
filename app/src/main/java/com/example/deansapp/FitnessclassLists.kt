1a139c242cc5b1628f4d7ca3285c0020c9bb9ef8
deanreidy <deanreidy24@gmail.com>
2019-03-04 12:17:28 +0000

    App with basic buttons before using work of practicals

--- app/src/main/java/com/example/deansapp/FitnessclassLists.kt
+++ app/src/main/java/com/example/deansapp/FitnessclassLists.kt
@@ -1,12 +1,33 @@
 package com.example.deansapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_fitnessclass_lists.*
 
 class FitnessclassLists : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fitnessclass_lists)
+
+        yogaBtn.setOnClickListener {
+            startActivity(Intent(this, Yoga::class.java))
+
+        }
+        hiitBtn.setOnClickListener {
+            startActivity(Intent(this, HIIT::class.java))
+
+        }
+        spinningBtn.setOnClickListener {
+            startActivity(Intent(this, Spinning::class.java))
+
+        }
+
+        addfitnessclassBtn.setOnClickListener {
+            startActivity(Intent(this, Spinning::class.java))
+
+        }
+
+        }
     }
-}
--- app/src/main/java/com/example/deansapp/Yoga.kt
+++ app/src/main/java/com/example/deansapp/Yoga.kt
@@ -1,12 +1,20 @@
 package com.example.deansapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_yoga.*
 
 class Yoga : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_yoga)
+
+        addyogainstructorBtn.setOnClickListener {
+            startActivity(Intent(this, addYogaInstructor::class.java))
+        }
+
     }
 }
